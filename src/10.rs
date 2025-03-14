06fca55cca139db8dc91171eb5de12cedf07c857
hardnettetoppins376 <hardnettetoppins376@gmail.com>
2025-03-14 17:12:55 +0000

    Add 10.rs

---
+++ src/10.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert("a", 1);
+    map.insert("b", 2);
+    map.insert("c", 3);
+    println!("{:?}", map);
+}
