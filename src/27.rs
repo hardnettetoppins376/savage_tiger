2e5b373f563d7ea147d3d3265977f1d9ec6143e5
hardnettetoppins376 <hardnettetoppins376@gmail.com>
2025-04-06 12:49:58 +0000

    Add 27.rs

---
+++ src/27.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Example Rust code to demonstrate various operations and idioms.
+    let x = 10;
+    println!("Value of x: {}", x);
+    if x > 5 {
+        println!("x is greater than 5");
+    }
+}
